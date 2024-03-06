f9fa4ad8ecc941cf15a427ccd2d4a7ce4e6d3912
rain46688 <rain46688@gmail.com>
2024-03-06 20:17:51 +0900

    5, 토큰 재발급 추가 및 Custom Hooks 생성(추가)

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,10 @@
 "use client"
-
-import { useLogin } from '../hooks/useLogin';
-// redux 관련 임포트
-import { setUser } from '../slices/userSlice';
+import Login from "../components/login/Login";
 
 export default function LoginPage() {
- 
-  // custom hook 사용
-  const { handleLogin, dispatch, user_id, password} = useLogin();
 
   return (
-    <main>
-      <form onSubmit={handleLogin}>
-        <h2>Login</h2>
-        <div>
-          <label>Id : </label>
-          <input
-            type="text"
-            value={user_id}
-            onChange={(event) => dispatch(setUser({ user_id: event.target.value, password }))}
-          />
-        </div>
-        <div>
-          <label>Password : </label>
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => dispatch(setUser({ user_id, password: event.target.value }))}
-          />
-        </div>
-        <div>
-          <button type="submit">Login</button>
-        </div>
-      </form>
-    </main>
+    // 로그인 페이지
+    <Login />
   );
 }
---
+++ components/login/Login.tsx
@@ -0,0 +1,37 @@
+import { useLogin } from '../../hooks/useLogin';
+// redux 관련 임포트
+import { setUser } from '../../slices/userSlice';
+
+// 로크인 컴포넌트
+export default function Login() {
+
+    // custom hook 사용
+    const { handleLogin, dispatch, user_id, password } = useLogin();
+
+    return (
+        <main>
+            <form onSubmit={handleLogin}>
+                <h2>Login</h2>
+                <div>
+                    <label>Id : </label>
+                    <input
+                        type="text"
+                        value={user_id}
+                        onChange={(event) => dispatch(setUser({ user_id: event.target.value, password }))}
+                    />
+                </div>
+                <div>
+                    <label>Password : </label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(event) => dispatch(setUser({ user_id, password: event.target.value }))}
+                    />
+                </div>
+                <div>
+                    <button type="submit">Login</button>
+                </div>
+            </form>
+        </main>
+    )
+}
